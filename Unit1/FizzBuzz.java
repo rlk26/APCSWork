d1a23fabfa0821194be64aa224f31ed0458d98c2
rlk26 <112580603+rlk26@users.noreply.github.com>
2022-09-15 09:38:31 -0700

    FizzBuzz

--- Unit1/FizzBuzz.java
+++ Unit1/FizzBuzz.java
@@ -1,16 +1,17 @@
-public class Multiples{
+public class FizzBuzz{
 
   public static void main(String[] args) {
-    for (int i = 0; i < 30; i++)
-    if (i%3 && i%5){
+    for (int i = 0; i < 30; i++){
+    if (i=%3 && i=%5){
       System.out.println("FizzBuzz");
     }
-    else if (i%3){
+    else if (i=%3){
       System.out.println("Fizz");
-    } else if (i%5){
+    } else if (i=%5){
       System.out.println("Buzz");
     } else {
         System.out.println(i);
     }
+  }
   }
 }
