93ddf286940942c1fb2cc28408cced1fbbed069c
rlk26 <112580603+rlk26@users.noreply.github.com>
2022-09-20 15:17:50 -0700

    hi

--- Unit1/Fibonacci.java
+++ Unit1/Fibonacci.java
@@ -1,11 +1,14 @@
 public class Fibonacci{
-//unfinished
   public static void main(String[] args) {
-    int a,b;
-    for (int i = 0; i < 30; i++){
-      a = i;
-      b = a+i;
-      System.out.println(b);
-  }
+    int a = 1;
+    int b = 1;
+    int c = 0;
+    for(int i = 1; i < 30; i++){
+      c = a+b;
+      System.out.println(c);
+      a = b;
+      b=c;
+
+    }
   }
 }
--- Unit1/FizzBuzz.java
+++ Unit1/FizzBuzz.java
@@ -2,12 +2,12 @@ public class FizzBuzz{
 
   public static void main(String[] args) {
     for (int i = 0; i < 30; i++){
-    if (i=%3 && i=%5){
+    if (i%3==0 && i%5==0){
       System.out.println("FizzBuzz");
     }
-    else if (i=%3){
+    else if (i%3==0){
       System.out.println("Fizz");
-    } else if (i=%5){
+    } else if (i%5==0){
       System.out.println("Buzz");
     } else {
         System.out.println(i);
