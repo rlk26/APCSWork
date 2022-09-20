33df539c84349871f6bcf1b6537aa3d910af8a59
rlk26 <112580603+rlk26@users.noreply.github.com>
2022-09-20 14:21:25 -0700

    Unit1 Stuff

---
+++ Unit1/Fibonacci.java
@@ -0,0 +1,11 @@
+public class Fibonacci{
+//unfinished
+  public static void main(String[] args) {
+    int a,b;
+    for (int i = 0; i < 30; i++){
+      a = i;
+      b = a+i;
+      System.out.println(b);
+  }
+  }
+}
---
+++ Unit1/Geometric.java
@@ -0,0 +1,8 @@
+public class Geometric{
+
+  public static void main(String[] args) {
+    for (int i = 3; i < 100; i*=2){
+      System.out.println(i);
+  }
+}
+}
