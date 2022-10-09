b65f412ed569fc6e88a6965d0babcc083b9a3982
rlk26 <112580603+rlk26@users.noreply.github.com>
2022-10-09 12:00:28 -0700

    Done with unit3

--- Unit3/dierolling.java
+++ Unit3/dierolling.java
@@ -1,7 +1,11 @@
 public class dierolling{
   public static void dierolling() {
-      double die = (int)(Math.random() + 6);
+      double die = (int)(Math.random() * 6);
+      if(die == 0.0){
+        System.out.println(6.0);
+      } else {
       System.out.println(die);
+    }
 }
 public static void main(String [] args){
   dierolling();
