f6f9ea64756fada61397b5df75c60449eb2a0eaf
rlk26 <112580603+rlk26@users.noreply.github.com>
2022-10-09 13:03:06 -0700

    r

---
+++ Unit4/Functions.java
@@ -0,0 +1,17 @@
+public class Functions {
+  public static void sumOfSquares(int sum){
+
+  }
+  public static void countOccurrences(String hey, String count){
+    int i = hey.indexOf(count);
+    
+  }
+  public static void reverse(){
+
+  }
+  public static void factorial(){
+
+  }
+  public static void main(String [] args){
+  }
+}
