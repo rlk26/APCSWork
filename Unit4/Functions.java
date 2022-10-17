b12e79e27026a269d5021c5857ef68e4a3e79e34
rlk26 <112580603+rlk26@users.noreply.github.com>
2022-10-17 08:38:57 -0700

    Update Functions.java

--- Unit4/Functions.java
+++ Unit4/Functions.java
@@ -1,15 +1,18 @@
 public class Functions {
-  /*public static void sumOfSquares(int sum){
-  }*/
-  public static void countOccurrences(String hey, String count){
+  public static void sumOfSquares(int sum){
+    if (int i = 0; i =< sum; i++)
+    i*i;
+  }
+}
+  /*public static void countOccurrences(String hey, String count){
     int counting = 0;
-    if(int i = 0; i<hey.length; i++){
+    if(int i = 0; i=<hey.length; i++){
       if(count == ""){
       counting++;
     }
   }
     return counting;
-  }
+  }*/
   /*public static void reverse(){
 
   }
@@ -18,6 +21,6 @@ public class Functions {
 
   }*/
   public static void main(String [] args){
-    countOccurrences("Bananananana, na");
+    //countOccurrences("Bananananana, na");
   }
 }
