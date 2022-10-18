d86f5df108e0df8ebf5b36f7e47dd286921b9b90
rlk26 <112580603+rlk26@users.noreply.github.com>
2022-10-18 10:14:31 -0700

    unit 6 stuff

---
+++ Unit6/TestProcessing.java
@@ -0,0 +1,24 @@
+import processing.core.*;
+
+public class TestProcessing extends PApplet
+{
+    public void settings()
+    {
+        size(400, 400);
+    }
+
+    public void setup()
+    {
+    }
+
+    public void draw()
+    {
+        background(0);
+        ellipse(200, 200, 100, 50);
+    }
+
+    public static void main(String[] args)
+    {
+        PApplet.main("TestProcessing");
+    }
+}
