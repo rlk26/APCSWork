d73b5950bc9c9429c483587506f0ee5fbebcf3d8
rlk26 <112580603+rlk26@users.noreply.github.com>
2023-01-09 09:27:00 -0800

    r

---
+++ Unit11/VehicleStats.java
@@ -0,0 +1,47 @@
+import java.util.*;
+public class VehicleStats{
+
+  public static void testtotalWheels(ArrayList<Vehicle>vehicles, int expected){
+    int correct = totalWheels(vehicles);
+    System.out.println("Expected: " + expected + " Correct: " + correct);
+    for(Vehicle v : vehicles){
+      System.out.println(v.wheelCount());
+    }
+    if(correct == expected){
+      System.out.println("Woohoo!");
+    } else {
+      System.out.println("Boohoo!");
+    }
+  }
+
+  public static int totalWheels(ArrayList<Vehicle>vehicles){
+    int result = 0;
+    for(Vehicle v : vehicles){
+      result += v.wheelCount();
+    }
+    return result;
+  }
+
+  public static ArrayList<Vehicle> humanPowered(ArrayList<Vehicle>vehicles){
+    int result = 0;
+    for(Vehicle v : vehicles){
+      result += v.wheelCount();
+    }
+    return result;
+  }
+
+  public static void main(String [] args){
+    ArrayList<Vehicle> vehicles = new ArrayList<Vehicle>();
+    Car c1 = new Car();
+    vehicles.add(c1);
+    Motorcycle m1 = new Motorcycle();
+    vehicles.add(m1);
+    Bicycle b1 = new Bicycle();
+    vehicles.add(b1);
+    Unicycle u1 = new Unicycle();
+    vehicles.add(u1);
+    testtotalWheels(vehicles,9);
+    humanPowered(vehicles);
+
+  }
+}
